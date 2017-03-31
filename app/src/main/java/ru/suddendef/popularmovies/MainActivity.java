342410a9634169a6ac7860e90a0c0a916e15a631
Andrew Slotin <andrew.slotin@gmail.com>
2017-03-31 23:36:11 +0200

    Add RecyclerView to MainActivity

--- app/src/main/java/ru/suddendef/popularmovies/MainActivity.java
+++ app/src/main/java/ru/suddendef/popularmovies/MainActivity.java
@@ -1,13 +1,26 @@
 package ru.suddendef.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+gimport android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private MovieDataAdapter moviesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+        moviesAdapter = new MovieDataAdapter(this, new ArrayList<TheMovieDbService.MovieData>());
+
+        RecyclerView moviePostersView = (RecyclerView) findViewById(R.id.rv_movie_posters);
+        moviePostersView.setLayoutManager(layoutManager);
+        moviePostersView.setAdapter(moviesAdapter);
+        moviePostersView.setHasFixedSize(true);
     }
 }
