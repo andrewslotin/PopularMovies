0e528b232dc9dbca31e398cae337ff1af4a29735
Andrew Slotin <andrew.slotin@gmail.com>
2017-04-01 00:05:29 +0200

    Add menu with Refresh buttor
    To be able to repload movies list for debugging purposes

--- app/src/main/java/ru/suddendef/popularmovies/MainActivity.java
+++ app/src/main/java/ru/suddendef/popularmovies/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -41,5 +44,22 @@ public class MainActivity extends AppCompatActivity {
 
         movieDb = new TheMovieDbService(getString(R.string.api_key));
         new MovieDbQuery().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                new MovieDbQuery().execute();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
